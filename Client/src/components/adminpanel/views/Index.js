51764cf2d81cd1f12ea2bc0af05d2e8020e2acd8
Muhammad Usman Aslam <41590975+aslam14@users.noreply.github.com>
2020-01-23 10:05:45 +0100

    Add files via upload
    Admin panel

---
+++ Client/src/components/adminpanel/views/Index.js
@@ -0,0 +1,58 @@
+import React from "react";
+// node.js library that concatenates classes (strings)
+import classnames from "classnames";
+// javascipt plugin for creating charts
+import Chart from "chart.js";
+// react plugin used to create charts
+import { Line, Bar } from "react-chartjs-2";
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  NavItem,
+  NavLink,
+  Nav,
+  Progress,
+  Table,
+  Container,
+  Row,
+  Col
+} from "reactstrap";
+
+// core components
+import {
+  chartOptions,
+  parseOptions,
+  chartExample1,
+  chartExample2
+} from "variables/charts.jsx";
+
+import Header from "components/Headers/Header.jsx";
+
+class Index extends React.Component {
+  state = {
+    activeNav: 1,
+    chartExample1Data: "data1"
+  };
+  
+  render() {
+    return (
+      <>
+        <Header />
+        {/* Page content */}
+        <Container className="mt--7" fluid>
+          <Row>
+            <Col className="mb-5 mb-xl-0" xl="8">
+              
+            </Col>
+              
+          </Row>
+        </Container>
+      </>
+    );
+  }
+}
+
+export default Index;
