e98f7872911ee0dd9cc1a0a18dc7db7325276809
rajan <rajanchawla3@gmail.com>
2020-02-17 13:47:47 +0100

    validation added for products

--- Client/src/components/PostItem.js
+++ Client/src/components/PostItem.js
@@ -9,7 +9,7 @@ import {
     Container,
     Row,
     Col,
-    Button, 
+    Button,
     Form,
     FormGroup,
     Label,
@@ -17,7 +17,7 @@ import {
     InputGroup,
     InputGroupAddon
 }
-from 'reactstrap';
+    from 'reactstrap';
 
 /**
  * CURRENT ISSUES:
@@ -40,7 +40,8 @@ class PostItem extends Component {
             location: '',
             category: '',
             imgFile: '',
-            categories: []  // for select dropdown
+            categories: [],  // for select dropdown
+            errors: {}
         };
 
         this.handleChange = this.handleChange.bind(this)
@@ -51,7 +52,7 @@ class PostItem extends Component {
     handleChange(event) {
         let stateName = event.target.name;
         console.log(event.target.value);
-        
+
         this.setState({
             [stateName]: event.target.value
         })
@@ -60,22 +61,23 @@ class PostItem extends Component {
 
     async handleSubmit(event) {
         event.preventDefault();
-        // get data from the submitted form.
-        const formData = new FormData(event.target);
+        if (this.validateForm()) {
+            // get data from the submitted form.
+            const formData = new FormData(event.target);
 
-        // get final URL from cloudinary.
-        const imgData= new FormData();
-        imgData.append("image", this.state.imgFile);
-        
-        await axios.post('/api/post/uploadpic', imgData)
-        .then(res => {
-            this.setState({
-                product_link: res.data.image
-            })
-        });
+            // get final URL from cloudinary.
+            const imgData = new FormData();
+            imgData.append("image", this.state.imgFile);
 
-        // process product submittion.
-        await axios.post('/api/post/addproduct', {  
+            await axios.post('/api/post/uploadpic', imgData)
+                .then(res => {
+                    this.setState({
+                        product_link: res.data.image
+                    })
+                });
+
+            // process product submittion.
+            await axios.post('/api/post/addproduct', {
                 description: formData.get('description'),
                 name: formData.get('name'),
                 quantity: formData.get('quantity'),
@@ -84,11 +86,55 @@ class PostItem extends Component {
                 seller_id: this.userId,      // update to logged in user id
                 category_id: formData.get('category'),
                 location: formData.get('location')
-        }).then(response => {
-            if (response.data.code == 200) {
-                this.props.history.push('/profile/history')    // redirect to url (maybe not best practice, need to recheck)
-            }
-        });
+            }).then(response => {
+                if (response.data.code == 200) {
+                    this.props.history.push('/profile/history')    // redirect to url (maybe not best practice, need to recheck)
+                }
+            });
+        }
+    }
+
+    validateForm() {
+        let name = this.state.name;
+        let price = this.state.price;
+        let quantity = this.state.quantity;
+        let description = this.state.description;
+        let imgFile = this.state.imgFile;
+        let formIsValid = true;
+        let errors = {};
+
+        if (!name) {
+            formIsValid = false;
+            window.alert("Please enter your name.");
+            errors["name"] = "Please enter your name.";
+        }
+        else
+        if (!price) {
+            formIsValid = false;
+            errors["price"] = "Please enter price.";
+            window.alert("Please enter price");
+        }
+        else
+        if (!quantity) {
+            formIsValid = false;
+            errors["quantity"] = "Please enter quantity";
+            window.alert("Please enter quantity");
+        }
+        else
+        if (!description) {
+            formIsValid = false;
+            errors["description"] = "*Please enter product description.";
+            window.alert("Please enter product description.");
+        }
+        else
+        if (!imgFile) {
+            formIsValid = false;
+            errors["imgFile"] = "*Please upload the image.";
+            window.alert("Please upload the image.");
+        }
+
+        return formIsValid;
+
     }
 
     getCategories = () => {
@@ -106,84 +152,85 @@ class PostItem extends Component {
 
         this.setState({
             imgFile: event.target.files[0]
-        }); 
+        });
     }
 
 
     componentWillMount() {
         this.getCategories();
     };
-            
-render() {
-    return (
-        <Container className={styles.postContainer}>
-            <div className={styles.pageHeader}>
-                <h2 className={styles.pageTitle}>Post a new product</h2>
-            </div>
-            <Form className={`${styles.postForm} boxShadow`} onSubmit={this.handleSubmit}>
-                <Row>
-                    <Col sm='4'>
-                        <FormGroup>
-                            <Label for="name">Name</Label>
-                            <Input type="text" name="name" id="name" placeholder="Product name"
-                                value={this.state.name} onChange={this.handleChange}/>
-                        </FormGroup>
-                    </Col>
-                    <Col sm='4'>
-                        <FormGroup>
-                            <Label for="location">Location</Label>
-                            <Input type="text" name="location" id="location" placeholder="Product location"
-                                value={this.state.location} onChange={this.handleChange}/>
-                        </FormGroup>
-                    </Col>
-                    <Col sm='4'>
-                        <FormGroup>
-                            <Label for="price">Price</Label>
-                            <InputGroup>
-                                <InputGroupAddon addonType="prepend">&euro;</InputGroupAddon>
-                                <Input type="number" name="price" id="price" placeholder="Price" min={0} step="1"
-                                    value={this.state.price} onChange={this.handleChange}/>
-                                <InputGroupAddon addonType="append">.00</InputGroupAddon>
-                            </InputGroup>
-                        </FormGroup>
-                    </Col>
-                    <Col sm='4'>
-                        <FormGroup>
-                            <Label for="quantity">Quantity</Label>
-                            <Input type="number" name="quantity" id="quantity" placeholder="Quantity" min={0} step="1"
-                                value={this.state.quantity} onChange={this.handleChange}/>
-                        </FormGroup>
-                    </Col>
-                    <Col sm='4'>
-                        <FormGroup>
-                            <Label for="location">Category</Label>
-                            <Input type="select" name="category" id="category"
-                                value={this.state.category} onChange={this.handleChange}>
-                                {this.state.categories.map((value, key) => <option value={value[1]} key={key}>{value[0]}</option>)}
-                            </Input>
-                        </FormGroup>
-                    </Col>
-                    <Col sm='8'>
-                        <FormGroup>
-                            <Label for="description">Description</Label>
-                            <Input type="textarea" name="description" id="description" placeholder="Describe your product as best as you can here." rows="6"
-                                value={this.state.description} onChange={this.handleChange}/>
-                        </FormGroup>
-                    </Col>                                   
-                    <Col sm='4'>
-                        <Label for="fileLoader">Load image</Label>
-                        <span className={styles.btnFile}>
-                            <Input type="file" name="fileLoader" id="fileLoader"  onChange={this.getFileImage}/>
-                        </span>
-                    </Col>
-                    <Col sm={{ size: 4, offset: 4 }}>
-                        <Button type="submit" value="Submit" className={`${styles.submitBtn} defaultBtn`}>Submit</Button>
-                    </Col>
-                </Row>
-            </Form>
-        </Container>
-    )
-  }
+
+    render() {
+        return (
+            <Container className={styles.postContainer}>
+                <div className={styles.pageHeader}>
+                    <h2 className={styles.pageTitle}>Post a new product</h2>
+                </div>
+                <Form className={`${styles.postForm} boxShadow`} onSubmit={this.handleSubmit}>
+                    <Row>
+                        <Col sm='4'>
+                            <FormGroup>
+                                <Label for="name">Name</Label>
+                                <Input type="text" name="name" id="name" placeholder="Product name"
+                                    value={this.state.name} onChange={this.handleChange} />
+                                      <div className="errorMsg">{this.state.errors.name}</div>
+                            </FormGroup>
+                        </Col>
+                        <Col sm='4'>
+                            <FormGroup>
+                                <Label for="location">Location</Label>
+                                <Input type="text" name="location" id="location" placeholder="Product location"
+                                    value={this.state.location} onChange={this.handleChange} />
+                            </FormGroup>
+                        </Col>
+                        <Col sm='4'>
+                            <FormGroup>
+                                <Label for="price">Price</Label>
+                                <InputGroup>
+                                    <InputGroupAddon addonType="prepend">&euro;</InputGroupAddon>
+                                    <Input type="number" name="price" id="price" placeholder="Price" min={0} step="1"
+                                        value={this.state.price} onChange={this.handleChange}  inputmode="numeric" pattern="[0-9]"/>
+                                    <InputGroupAddon addonType="append">.00</InputGroupAddon>
+                                </InputGroup>
+                            </FormGroup>
+                        </Col>
+                        <Col sm='4'>
+                            <FormGroup>
+                                <Label for="quantity">Quantity</Label>
+                                <Input type="number" name="quantity" id="quantity" placeholder="Quantity" min={0} step="1"
+                                    value={this.state.quantity} onChange={this.handleChange} />
+                            </FormGroup>
+                        </Col>
+                        <Col sm='4'>
+                            <FormGroup>
+                                <Label for="location">Category</Label>
+                                <Input type="select" name="category" id="category"
+                                    value={this.state.category} onChange={this.handleChange}>
+                                    {this.state.categories.map((value, key) => <option value={value[1]} key={key}>{value[0]}</option>)}
+                                </Input>
+                            </FormGroup>
+                        </Col>
+                        <Col sm='8'>
+                            <FormGroup>
+                                <Label for="description">Description</Label>
+                                <Input type="textarea" name="description" id="description" placeholder="Describe your product as best as you can here." rows="6"
+                                    value={this.state.description} onChange={this.handleChange} />
+                            </FormGroup>
+                        </Col>
+                        <Col sm='4'>
+                            <Label for="fileLoader">Load image</Label>
+                            <span className={styles.btnFile}>
+                                <Input type="file" name="fileLoader" id="fileLoader" onChange={this.getFileImage} />
+                            </span>
+                        </Col>
+                        <Col sm={{ size: 4, offset: 4 }}>
+                            <Button type="submit" value="Submit" className={`${styles.submitBtn} defaultBtn`}>Submit</Button>
+                        </Col>
+                    </Row>
+                </Form>
+            </Container>
+        )
+    }
 }
 
 export default withRouter(PostItem);
