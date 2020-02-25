a651eb1b68247d6b71eaf663dc18fee482203c44
BlinVarfi <blinvarfi@gmail.com>
2020-02-25 19:41:13 +0100

    Fixing minor bug

--- Client/src/components/ProductView.js
+++ Client/src/components/ProductView.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { withRouter, Link } from 'react-router-dom';
 import axios from 'axios';
-import Rater from './Rater';
 import RatingStars from './RatingStars';
 
 import styles from './componentStyles/ProductView.module.css';
